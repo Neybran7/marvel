925f2e6ab70fe012fb17ad80897642d85ab2c73e
Neybran7 <neybran@gmail.com>
2023-04-24 18:15:01 -0500

    Actualizando el repositorio

--- appimarvel/comics/urls.py
+++ appimarvel/comics/urls.py
@@ -10,6 +10,4 @@ urlpatterns=[
     path('register/', views.register, name='register'),
     path('login/',LoginView.as_view(template_name='social/login.html'), name='login'),
     path('logout/',LogoutView.as_view(template_name='social/logout.html'),name='logout'),    
-   
-     
 ]
--- appimarvel/comics/views.py
+++ appimarvel/comics/views.py
@@ -1,4 +1,4 @@
-#import requests
+import requests
 import json
 from django.shortcuts import redirect, render
 from .models import *
@@ -8,8 +8,23 @@ from django.contrib import messages
 
 def inicio(request):
     #return render(request, 'social/feed.html', {'nombre':nombre})
-    nombre = "Neybran"
-    return render(request, 'social/feed.html' , {'nombre':nombre})
+    private_key = "ae44c373a40cae4d4393c7eb5af469e3324f0c82"
+    public_key = "620661ca732f53b2798f48cfef87e595"
+    hashed = "2a47b76bda75a7fcf941a067b556ea90"
+    laurl = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=620661ca732f53b2798f48cfef87e595&hash=2a47b76bda75a7fcf941a067b556ea90"
+    lista = []
+    response = requests.get(laurl)
+    if response.status_code==200:
+        response_json = json.loads(response.text)
+        for i in response_json["data"]["results"]:
+            idcomic = i["id"]
+            nombre = i["name"]
+            imagen = i["thumbnail"]["path"]
+            extension = i["thumbnail"]["extension"]
+            dic = {"nombre":nombre,"imagen":imagen,"extension":extension,"id":idcomic}
+            lista.append(dic)
+
+    return render(request, 'social/feed.html' , {'comics_marvel':lista})
 
 def profile(request):
     #aca ya esta logeado ? si
